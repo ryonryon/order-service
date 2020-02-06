2b74481e45bc37aef9d783a7e25c74a9d775afbb
ryotogashi <ryo.togashi.ca@gmail.com>
2020-02-06 13:05:56 -0800

    delete comment.

--- src/repositories/orderRepository.ts
+++ src/repositories/orderRepository.ts
@@ -2,7 +2,10 @@ import { RunResult } from "sqlite3";
 
 import dBSqlite3 from "../db/dbSqlite3";
 import { updateInventoryItemQuantiy } from "../db/invnetoryQueries";
-import { insertOrderDetail, createOrderDetailTable } from "../db/orderDetailQueries";
+import {
+  insertOrderDetail,
+  createOrderDetailTable
+} from "../db/orderDetailQueries";
 import {
   createOrderTable,
   selectOrders,
@@ -31,11 +34,26 @@ class OrderTable {
         console.log(row);
         if (err) throw err;
         orderItems.forEach(orderItem => {
-          console.log(updateInventoryItemQuantiy(orderItem.inventoryId, orderItem.newQuantityAvailable));
-          console.log(insertOrderDetail(row["order_id"], orderItem.inventoryId, orderItem.quantity));
-          db.run(updateInventoryItemQuantiy(orderItem.inventoryId, orderItem.newQuantityAvailable));
+          console.log(
+            updateInventoryItemQuantiy(
+              orderItem.inventoryId,
+              orderItem.newQuantityAvailable
+            )
+          );
+          console.log(
+            insertOrderDetail(
+              row["order_id"],
+              orderItem.inventoryId,
+              orderItem.quantity
+            )
+          );
           db.run(
-            // TODO
+            updateInventoryItemQuantiy(
+              orderItem.inventoryId,
+              orderItem.newQuantityAvailable
+            )
+          );
+          db.run(
             insertOrderDetail(1, orderItem.inventoryId, orderItem.quantity)
           );
         });
@@ -45,12 +63,16 @@ class OrderTable {
 
   static getOrders(): Promise<any> {
     const db = dBSqlite3();
-    return new Promise((resolve, reject) => db.all(selectOrders(), (err, rows) => (err ? reject(err) : resolve(rows))));
+    return new Promise((resolve, reject) =>
+      db.all(selectOrders(), (err, rows) => (err ? reject(err) : resolve(rows)))
+    );
   }
 
   static getOrder(id: Number): Promise<any> {
     const db = dBSqlite3();
-    return new Promise((resolve, reject) => db.get(selectOrder(id), (err, row) => (err ? reject(err) : resolve(row))));
+    return new Promise((resolve, reject) =>
+      db.get(selectOrder(id), (err, row) => (err ? reject(err) : resolve(row)))
+    );
   }
 
   static updateOrder(
@@ -71,7 +93,9 @@ class OrderTable {
   static deleteOrder(id: Number): Promise<void> {
     const db = dBSqlite3();
     return new Promise((resolve, reject) =>
-      db.run(deleteOrder(id), (err: Error | null, _: RunResult) => (err ? reject(err) : resolve()))
+      db.run(deleteOrder(id), (err: Error | null, _: RunResult) =>
+        err ? reject(err) : resolve()
+      )
     );
   }
 }
