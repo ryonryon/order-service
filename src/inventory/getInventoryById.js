46c4afeb913d8934a0037a0333d76d929f429e5e
ryotogashi <ryo.togashi.ca@gmail.com>
2020-02-05 20:19:31 -0800

    inventoryId -> id

--- src/inventory/getInventoryById.js
+++ src/inventory/getInventoryById.js
@@ -2,15 +2,15 @@ import InventoryTable from "../db/InventoryTable";
 import { INVALID_PARAM_ERROR } from "../constants";
 
 async function getInventoryById(req, res) {
-  const inventryId = req.params.id;
+  const id = req.params.id;
   try {
-    const inventry = await InventoryTable.getInventory(inventryId);
+    const inventry = await InventoryTable.getInventory(id);
     if (inventry === undefined) throw INVALID_PARAM_ERROR.type;
 
     res.status(200).send(inventry);
   } catch (err) {
     if (err === INVALID_PARAM_ERROR.type)
-      res.status(400).send(INVALID_PARAM_ERROR.message(inventryId));
+      res.status(400).send(INVALID_PARAM_ERROR.message(id));
     else res.status(500).send(INVALID_PARAM_ERROR.message());
   }
 }
